8561055b589d9aef930e9a41944999eca4658f35
johnandblue <jc@johnandblue.com>
2017-01-11 18:06:08 +0100

    added route sign-in

--- server/router.js
+++ server/router.js
@@ -1,7 +1,7 @@
 const router = require('koa-router')();
 const controller = require('./controllers/auth.controller.js')
 
-// router
-//   .get('/sign-in', authController.signIn);
+router
+  .get('/sign-in', controller.signIn);
 
 module.exports = router;
