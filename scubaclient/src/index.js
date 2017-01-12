9b46d5224629302d03f4b47341d514c801a4c1ac
johnandblue <jc@johnandblue.com>
2017-01-12 17:30:48 +0100

    chore react && redux

--- scubaclient/src/index.js
+++ scubaclient/src/index.js
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+console.log();
+
 ReactDOM.render(
-  <App />,
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>,
   document.getElementById('root')
 );
